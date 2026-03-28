d051fc0b82ba1adfcfc95610dfc1a02a5b2d1beb
Paddione <patrick@korczewski.de>
2026-03-28 17:31:13 +0000

    1

--- scripts/import-entrypoint.sh
+++ scripts/import-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Substituiert Umgebungsvariablen in realm-homeoffice.json
 # und startet Keycloak mit --import-realm
+#
+# Hinweis: envsubst ist im Keycloak-Image (RHEL UBI9-micro) nicht
+# verfügbar, daher werden die Variablen per sed ersetzt.
 set -e
 
 TEMPLATE="/opt/keycloak/realm-template/realm-homeoffice.json"
@@ -8,8 +11,18 @@ OUTPUT="/opt/keycloak/data/import/realm-homeoffice.json"
 
 mkdir -p "$(dirname "$OUTPUT")"
 
-# Alle ${VAR} Referenzen im JSON durch aktuelle Env-Werte ersetzen
-envsubst < "$TEMPLATE" > "$OUTPUT"
+# Alle ${VAR} Referenzen im JSON durch aktuelle Env-Werte ersetzen (sed-basiert)
+cp "$TEMPLATE" "$OUTPUT"
+for var in MATTERMOST_OIDC_SECRET NEXTCLOUD_OIDC_SECRET \
+           MM_DOMAIN NC_DOMAIN \
+           LLDAP_BASE_DOMAIN LLDAP_BASE_TLD LLDAP_LDAP_USER_PASS; do
+  eval val="\${${var}:-}"
+  if [ -z "$val" ]; then
+    echo "[import-entrypoint] WARNUNG: ${var} ist nicht gesetzt!"
+  else
+    sed -i "s|\${${var}}|${val}|g" "$OUTPUT"
+  fi
+done
 
 echo "[import-entrypoint] Realm JSON generiert: $OUTPUT"
 
